06ed146a03e687641092c79b0586edb510647fae
TomisTony <1158176767@qq.com>
2024-01-03 15:15:13 +0800

    引入 tailwindcss/antd

--- frontend/web-cad/src/App.tsx
+++ frontend/web-cad/src/App.tsx
@@ -2,16 +2,24 @@ import React, { useEffect } from "react"
 import { ThreeApp } from "./ThreeApp"
 import { Shape } from "./Shape"
 
+import { Button } from "antd"
+
 import getOutput from "./output.temp"
 
 function App() {
   const threeApp = ThreeApp.getInstance()
-  useEffect(() => {
+  const loadModel = () => {
     getOutput().then((output) => {
       new Shape().setBrCADToScene(output)
     })
-  }, [])
-  return <></>
+  }
+  return (
+    <>
+      <Button type="primary" onClick={loadModel}>
+        加载模型
+      </Button>
+    </>
+  )
 }
 
 // import output from "./output.temp"
