4efe505d1602eeb9b386d23212663a6ada715e95
TomisTony <1158176767@qq.com>
2024-01-28 15:27:12 +0800

    添加了对于要操作的历史记录的提示

--- frontend/web-cad/src/Components/History/index.tsx
+++ frontend/web-cad/src/Components/History/index.tsx
@@ -1,16 +1,23 @@
 import React from "react"
 import HistoryList from "./components/HistoryList"
 import FunctionalButtonList from "./components/FunctionalButtonList"
+import { useAppSelector } from "@/app/hooks"
 
 interface HistoryProps {
   className?: string
 }
 
 function History(props: HistoryProps) {
+  const choosedHistoryIndex = useAppSelector(
+    (state) => state.history.choosedHistoryIndex,
+  )
+  const historyList = useAppSelector((state) => state.history.historyList)
+  const choosedHistoryName = historyList[choosedHistoryIndex]?.operationName
   return (
     <div
       className={
-        "bg-gray-600 bg-opacity-50 p-1 flex justify-between space-x-2 " + props.className
+        "bg-gray-600 bg-opacity-50 p-1 flex justify-between space-x-2 " +
+        props.className
       }
     >
       <div className="flex flex-col flex-1">
@@ -19,7 +26,9 @@ function History(props: HistoryProps) {
       </div>
       <div className="w-[1px] bg-gray-300" />
       <div className="flex flex-col w-[20%]">
-        <div className="m-1 text-white font-bold">History Operation</div>
+        <div className="m-1 text-white font-bold">
+          Choosed History: {choosedHistoryName ?? "None"}
+        </div>
         <FunctionalButtonList className="m-1 mr-4 flex-1" />
       </div>
     </div>
