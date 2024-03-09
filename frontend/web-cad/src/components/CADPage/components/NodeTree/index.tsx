e61873fec24536c15ce0476f8d2cfbc19cbd0414
TomisTony <1158176767@qq.com>
2024-03-09 17:39:11 +0800

    NodeTree 样式完成

--- frontend/web-cad/src/components/CADPage/components/NodeTree/index.tsx
+++ frontend/web-cad/src/components/CADPage/components/NodeTree/index.tsx
@@ -1,12 +1,62 @@
-import React from "react"
+import React, { useRef, useEffect } from "react"
+import { Tree, TreeDataNode } from "antd"
+import { useAppDispatch, useAppSelector } from "@/app/hooks"
+import { BrCADNode } from "@/types/BrCAD"
 
 interface NodeTreeProps {
   className?: string
 }
 
 function NodeTree(props: NodeTreeProps) {
+  const dispatch = useAppDispatch()
+  const ref = useRef<HTMLDivElement>(null)
+  const brcad = useAppSelector((state) => state.model.model)
+  const brcadStructure = brcad?.structure
+  const [treeHight, setTreeHight] = React.useState(0)
+  useEffect(() => {
+    if (ref.current) {
+      // 获取所有样式
+      const style = window.getComputedStyle(ref.current)
+
+      // 获取元素实际的高度（包括padding）
+      const totalHeight = ref.current.clientHeight
+
+      // 提取padding值
+      const paddingTop = parseFloat(style.getPropertyValue("padding-top"))
+      const paddingBottom = parseFloat(style.getPropertyValue("padding-bottom"))
+
+      // 计算并打印不包括padding的高度
+      const heightWithoutPadding = totalHeight - paddingTop - paddingBottom
+      setTreeHight(heightWithoutPadding)
+    }
+  }, [ref])
+
+  const makeDisabledTreeDataNode = (node: BrCADNode): TreeDataNode => {
+    return {
+      title: node.label,
+      key: node.id,
+      disabled: true,
+      children: node.children.map(makeDisabledTreeDataNode),
+    }
+  }
+
+  const treeData: TreeDataNode[] = [
+    {
+      title: "root",
+      key: "root",
+      children: brcadStructure?.children.map((node) => {
+        return {
+          title: node.label,
+          key: node.id,
+          children: node.children.map(makeDisabledTreeDataNode),
+        }
+      }),
+    },
+  ]
+
   return (
     <div
+      ref={ref}
       className={
         "bg-gray-400 bg-opacity-70 w-[18%] p-4 flex flex-col shadow-sm" +
         " select-none pointer-events-auto " +
@@ -15,7 +65,12 @@ function NodeTree(props: NodeTreeProps) {
         props.className
       }
     >
-      NodeTree
+      <Tree
+        treeData={treeData}
+        defaultExpandedKeys={["root"]}
+        height={treeHight} // 获取 ref 元素的高度
+        className="bg-white bg-opacity-30"
+      />
     </div>
   )
 }
